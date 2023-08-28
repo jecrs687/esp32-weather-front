d04e3bf26776404b68103c074920202025cfb957
Epaminondas Noronha Feitosa <nondasnoronha@gmail.com>
2023-08-27 22:59:46 -0300

    add api fetch function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 import styles from './page.module.css'
 
-export default function Home() {
+async function getData() {
+  const res = await fetch('https://top-redes-40e9a5ec3e40.herokuapp.com/')
+  const data = await res.json()
+  return data
+}
+
+export default async function Home() {
+  const data = await getData()
   return (
     <main className={styles.main}>
-      
+      {JSON.stringify(data)}
     </main>
   )
 }
