a2a0ffae9aa54f12fac4db0668e3fac12eb908be
fernando <fernandomirandalolzeiro@gmail.com>
2023-08-27 23:11:19 -0300

    Add style to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import styles from './page.module.css'
+import styles from './wheater.module.css'
 
 async function getData() {
   const res = await fetch('https://top-redes-40e9a5ec3e40.herokuapp.com/')
@@ -8,9 +8,20 @@ async function getData() {
 
 export default async function Home() {
   const data = await getData()
+
   return (
-    <main className={styles.main}>
-      {JSON.stringify(data)}
-    </main>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Estação Meteorológica</h1>
+      <div className={styles.dataContainer}>
+        <div className={styles.dataPoint}>
+          <span className={styles.icon}>🌡️</span>
+          <span>Temperatura: <span className={styles.data}>15°C</span></span>
+        </div>
+        <div className={styles.dataPoint}>
+          <span className={styles.icon}>💧</span>
+          <span>umidade: <span className={styles.data}>15%</span></span>
+        </div>
+      </div>
+    </div>
   )
 }
