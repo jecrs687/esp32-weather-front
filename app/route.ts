049b668607699b0af395e089492f8d9d87a1c162
Emanuel Cascone <emanuelcascone@gmail.com>
2023-08-29 08:10:26 -0300

    fix: remove route

--- app/route.ts
+++
@@ -1,8 +0,0 @@
-
-export async function POST(request: Request) {
-    const body = await request.json()
-    const { name } = body
-    return new Response(`Hello ${name}!`, {
-        headers: { 'content-type': 'text/plain' },
-    })
-}
