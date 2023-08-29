d4bf2d639c52ceaff2d5898bf1e93b288dcebed4
Emanuel Cascone <emanuelcascone@gmail.com>
2023-08-28 23:54:03 -0300

    feat: new chart

--- src/services/home/index.tsx
+++ src/services/home/index.tsx
@@ -1,4 +1,5 @@
 'use client';
+import dynamic from 'next/dynamic'
 import LoadingSpinner from '@/components/Spinner/spinner';
 import styles from './wheater.module.css'
 import React, { Ref, useEffect, useState } from 'react'
@@ -9,8 +10,10 @@ import { rain } from '@/utils/createRain';
 import { firstLetterToUpperCase } from '@/utils/firtsLetterToUpperCase';
 import { TimerComponent } from '@/components/Timer/timer';
 // @ts-ignore
-import  Thermometer from "react-thermometer-chart";
+import Thermometer from "react-thermometer-chart";
+const Chart = dynamic(() => import('react-apexcharts'), { ssr: false });
 
+import { optionsChart } from './options';
 type Data = {
     temperature: number,
     humidate: number,
@@ -25,7 +28,6 @@ async function getData() {
     return data
 
 }
-
 export const HomeComponent = () => {
     const { data, isLoading } = useSWR('/api/user', {
         refreshInterval: 1000,
@@ -33,7 +35,7 @@ export const HomeComponent = () => {
     })
 
     const elRef = React.useRef(null)
-    if(elRef.current) rain(elRef)
+    if (elRef.current) rain(elRef)
 
 
     return (
@@ -45,52 +47,60 @@ export const HomeComponent = () => {
                 }
             }
         >
-            <section className={styles.rain} ref={elRef}/>
+            <section className={styles.rain} ref={elRef} />
             <h1 className={styles.title}>Estação Meteorológica</h1>
             <h1 className={styles.title}></h1>
 
             <div
-            className={styles.dataBox}
-            >        
+                className={styles.dataBox}
+            >
 
-            <Thermometer
-                width="150px"
-                height="250px"
-                steps={5}
-                color={
-                    "rgb(" + getBackground(!isLoading ? data.temperature : 30) + ")"
-                }
-                minValue={0}
-                maxValue={45}
-                currentValue={data?.temperature || 20}
-            />
-            <div className={styles.dataContainer}>
-            <TimerComponent/>
-                {isLoading ? <LoadingSpinner /> :
-                    <>
-                        <div className={styles.dataPoint}>
-                            <span className={styles.icon}>🌡️</span>
-                            <span className={styles.data}>Temperatura: {data.temperature}°C</span>
-                        </div>
-                   
-                        <div className={styles.dataPoint}>
-                            <span className={styles.icon}>💧</span>
-                            <span className={styles.data}>Umidade: {data.humidate}%</span>
-                        </div>
-                        <div className={styles.dataPoint}>
-                            <span className={styles.icon}>🌧️</span>
-                            <span className={styles.data}>Molhado: {data.molhado ? 'Sim' : 'Não'}
-                            </span>
-                        </div>
-                        <div className={styles.dataPoint}>
-                            <span className={styles.icon}>😖</span>
-                            <span className={styles.data}>Tremores: {data.shaking}/{data.captures}
-                            </span>
-                        </div>
-                    </>
-                }
-            </div>
+                <Thermometer
+                    width="150px"
+                    height="250px"
+                    steps={5}
+                    color={
+                        "rgb(" + getBackground(!isLoading ? data.temperature : 30) + ")"
+                    }
+                    minValue={0}
+                    maxValue={45}
+                    currentValue={data?.temperature || 20}
+                />
+                <div className={styles.dataContainer}>
+                    <TimerComponent />
+                    {isLoading ? <LoadingSpinner /> :
+                        <>
+                            <div className={styles.dataPoint}>
+                                <span className={styles.icon}>🌡️</span>
+                                <span className={styles.data}>Temperatura: {data.temperature}°C</span>
+                            </div>
+
+                            <div className={styles.dataPoint}>
+                                <span className={styles.icon}>💧</span>
+                                <span className={styles.data}>Umidade: {data.humidate}%</span>
+                            </div>
+                            <div className={styles.dataPoint}>
+                                <span className={styles.icon}>🌧️</span>
+                                <span className={styles.data}>Molhado: {data.molhado ? 'Sim' : 'Não'}
+                                </span>
+                            </div>
+                            <div className={styles.dataPoint}>
+                                <span className={styles.icon}>😖</span>
+                                <span className={styles.data}>Tremores: {data.shaking}/{data.captures}
+                                </span>
+                            </div>
+                        </>
+                    }
+                </div>
+               {(typeof window !== 'undefined') && <Chart
+                series={[data?.humidate || 20]}
+                options={optionsChart}
+                type="radialBar"
+                height={350}
+                />}
+                
             </div>
         </div>
     )
 }
+
---
+++ src/services/home/options.tsx
@@ -0,0 +1,43 @@
+export const optionsChart ={
+  chart: {
+    height: 350,
+    type: 'radialBar',
+    offsetY: -10
+  },
+  plotOptions: {
+    radialBar: {
+      startAngle: -135,
+      endAngle: 135,
+      dataLabels: {
+        name: {
+          fontSize: '16px',
+          color: undefined,
+          offsetY: 120
+        },
+        value: {
+          offsetY: 76,
+          fontSize: '22px',
+          color: undefined,
+          formatter: function (val: number) {
+            return val + "%";
+          }
+        }
+      }
+    }
+  },
+  fill: {
+    type: 'gradient',
+    gradient: {
+        shade: 'dark',
+        shadeIntensity: 0.15,
+        inverseColors: false,
+        opacityFrom: 1,
+        opacityTo: 1,
+        stops: [0, 50, 65, 91]
+    },
+  },
+  stroke: {
+    dashArray: 4
+  },
+  labels: ['Humidade'],
+}
